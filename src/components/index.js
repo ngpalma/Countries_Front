7f76f92d8428bf0b364bb8727f06b78dc7373008
ngpalma <nicolasgerardopalma@gmail.com>
2023-07-22 13:19:33 -0300

    actualizacion

--- src/components/index.js
+++ src/components/index.js
@@ -3,4 +3,4 @@ import CardsContainer from "./Cards Container/CardsContainer.jsx";
 import NavBar from "./Nav Bar/NavBar.jsx";
 import SearchBar from "./Search Bar/SearchBar.jsx";
 
-export { Card, CardsContainer, NavBar, SearchBar };
+export { Card, CardsContainer, NavBar, SearchBar }
